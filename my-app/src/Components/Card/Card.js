81f48a6876f52036325bb6599fa1bd7b074093ca
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-02-15 17:20:26 -0500

    Card component mockup finished.

--- my-app/src/Components/Card/Card.js
+++ my-app/src/Components/Card/Card.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { Card, Text, Row, Grid } from "@nextui-org/react";
+import { Grid, Card, Row, Text, Button } from "@nextui-org/react";
+import Avocado from '../../Assets/Images/fruit-5.jpeg';
 
 function CardComponent() {
   return (
-    <Grid xs={6} sm={3}>
-      <Card hoverable clickable color={'gradient'}>
+    <Grid sm={2}>
+      <Card clickable bordered>
+        <Card.Header>
+          <Card.Image
+            src={Avocado}
+            width="100%"
+            height={140}
+            objectFit='cover'
+          />
+        </Card.Header>
         <Card.Body>
-          <Card.Image />
-        </Card.Body>
-        <Card.Footer justify="flex-start">
-          <Row wrap='wrap' justify='space-between'>
+          <Row justify='center'>
             <Text b>Item name would be here!</Text>
+          </Row>
+          <Row justify='center'>
             <Text css={{ color: "$accents4", fontWeight: "$semibold" }}> $15.99 </Text>
+          </Row>
+        </Card.Body>
+        <Card.Footer>
+          <Row justify='center'>
+            <Button rounded color="error">Comprar</Button>
           </Row>
         </Card.Footer>
       </Card>
--- my-app/src/Components/Navbar/Navbar.js
+++ my-app/src/Components/Navbar/Navbar.js
@@ -18,7 +18,6 @@ function Navbar() {
           </Col>
         </Row>
       </Card>
-
     </Container >
   )
 }
