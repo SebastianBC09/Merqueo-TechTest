3830bde8a63784fe147fd65b838cbc71f801176f
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-02-15 17:59:56 -0500

    Provider added to index file in order to access data from context.

--- my-app/src/App/index.js
+++ my-app/src/App/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { AppUI } from './AppUI';
 import { NextUIProvider } from '@nextui-org/react';
+import { Provider } from "../Context/index"
 import './App.css';
 
 function App() {
   return (
     <NextUIProvider>
-      <AppUI />
+      <Provider>
+        <AppUI />
+      </Provider>
     </NextUIProvider>
   );
 }
