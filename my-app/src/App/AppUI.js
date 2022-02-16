9afc7eb3e0ea6f689d3d67484de6f64ea2e34b2c
Sebastian Ballen <sebastian.ballenc@gmail.com>
2022-02-16 09:46:18 -0500

    Mapping of data from API made. Attributes of each item send to children in order to render the corresponding info.

--- my-app/src/App/AppUI.js
+++ my-app/src/App/AppUI.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Container, Grid } from "@nextui-org/react";
+import { Context } from "../Context";
 import { Navbar } from "../Components/Navbar/Navbar"
 import { CardComponent } from "../Components/Card/Card"
+import { Container, Grid } from "@nextui-org/react";
 
 function AppUI() {
+  const { items } = React.useContext(Context);
+
   return (
     <>
       <Navbar />
       <Container fluid>
         <Grid.Container gap={2}>
-          <Grid xs={12} sm={3} md={3}>
-            <CardComponent />
-          </Grid>
+          {items.map((items, id) => (
+            <Grid xs={12} sm={3} md={3} key={id}>
+              <CardComponent attributes={items.attributes} />
+            </Grid>
+          ))}
         </Grid.Container>
       </Container>
     </>
--- my-app/src/Components/Card/Card.js
+++ my-app/src/Components/Card/Card.js
@@ -2,23 +2,25 @@ import React from 'react';
 import { Card, Row, Text, Button } from "@nextui-org/react";
 import Avocado from '../../Assets/Images/fruit-5.jpeg';
 
-function CardComponent() {
+function CardComponent(props) {
+  const { attributes } = props;
+  console.log(attributes)
   return (
     <Card clickable bordered>
       <Card.Header>
         <Card.Image
-          src={Avocado}
+          src={attributes.image_large_url}
           width="100%"
           height={140}
-          objectFit='cover'
+        // objectFit='cover'
         />
       </Card.Header>
       <Card.Body>
         <Row justify='center'>
-          <Text b>Item name would be here!</Text>
+          <Text b>{attributes.name}</Text>
         </Row>
         <Row justify='center'>
-          <Text css={{ color: "$accents4", fontWeight: "$semibold" }}> $15.99 </Text>
+          <Text css={{ color: "$accents4", fontWeight: "$semibold" }}>$ {attributes.price}</Text>
         </Row>
       </Card.Body>
       <Card.Footer>
